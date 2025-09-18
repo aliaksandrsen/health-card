cac8cec527c2f6ad94062eab8475dc82fc7d36e4
Alex <puskin1914@gmail.com>
2025-09-19 00:11:56 +0400

    Update signOut button

--- src/app/(logged-in)/SignOutButton.tsx
+++ src/app/(logged-in)/SignOutButton.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { LogOut } from 'lucide-react';
 import { signOut } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 
 export const SignOutButton = () => {
   return (
-    <Button variant="destructive" onClick={() => signOut()}>
-      Sign Out
+    <Button variant="outline" onClick={() => signOut()}>
+      <LogOut />
     </Button>
   );
 };

732d8d580aa281e25119d8ece9571b244f48cca2
Alex <puskin1914@gmail.com>
2025-09-30 20:49:06 +0400

    refactor(layout): remove UserProvider and simplify layout structure

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@/auth';
 import { Header } from './components/Header';
-import { UserProvider } from './user-context';
 
 export default async function LoggedInLayout({
   children,
@@ -14,14 +13,10 @@ export default async function LoggedInLayout({
     redirect('/login');
   }
 
-  const userId = Number.parseInt(session.user.id, 10);
-
   return (
-    <UserProvider value={{ userId }}>
-      <div className="flex min-h-screen flex-col bg-background">
-        <Header user={session.user} />
-        <main className="flex flex-1 flex-col">{children}</main>
-      </div>
-    </UserProvider>
+    <div className="flex min-h-screen flex-col bg-background">
+      <Header user={session.user} />
+      <main className="flex flex-1 flex-col">{children}</main>
+    </div>
   );
 }

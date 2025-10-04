847a34310e4fdcc291fe4fdcfcfc1575db896194
Alex <puskin1914@gmail.com>
2025-10-05 00:45:05 +0400

    chore: add postinstall script for Prisma generation and update migrate:deploy command

--- prisma/migrations/20251004173750_test/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."users" RENAME CONSTRAINT "User_pkey" TO "users_pkey";
-
--- AlterTable
-ALTER TABLE "public"."visits" RENAME CONSTRAINT "Visit_pkey" TO "visits_pkey";

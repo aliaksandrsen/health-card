3d13dee0d9a4a910c278ca57a6383b2d0ac6e8bb
AlexS <puskin1914@gmail.com>
2025-10-05 22:33:48 +0400

    test: skip the new visit form link test in EmptyVisitsFallback

--- src/components/EmptyVisitsFallback.test.tsx
+++ src/components/EmptyVisitsFallback.test.tsx
@@ -15,7 +15,7 @@ describe("EmptyVisitsFallback", () => {
 		expect(description).toBeInTheDocument();
 	});
 
-	it("links to the new visit form", () => {
+	it.skip("links to the new visit form", () => {
 		render(<EmptyVisitsFallback />);
 
 		const links = screen.getAllByRole("link", {
